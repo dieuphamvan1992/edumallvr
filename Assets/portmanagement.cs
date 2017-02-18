b5e9a9cc63ea084de41140dfb5825c88d30f70a4
Hồng Lâm <Hng Lm>
2017-02-18 22:39:52 +0700

    done gateway

--- Assets/portmanagement.cs
+++ Assets/portmanagement.cs
@@ -1,22 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.SceneManagement;
 
 public class portmanagement : MonoBehaviour {
-    private float ypos = 250;
-    private float endposz = -28;
-    private float moverate = 1f;
+    //private float ypos = 250;
+    private float endposz = -30;
+    private float moverate = 0.3f;
+    public GameObject cube;
  	void Start () {
         //ypos = this.gameObject.transform.position.x;
         //Debug.Log(ypos.ToString());
 	}
 	
 	// Update is called once per frame
-	void Update () {        
-        if (this.gameObject.transform.position.z <endposz)
+	void Update () {
+        if (this.gameObject.transform.position.z < endposz)
         {
             this.gameObject.transform.position += new Vector3(0, 0, moverate);
             //this.gameObject.transform.eulerAngles += new Vector3(0,0,15);            
         }
+        else
+        {
+            //this.gameObject.transform.eulerAngles += new Vector3(0, 0, 15);
+            StartCoroutine(Rotehole());
+        }
 	}
+
+    IEnumerator Rotehole()
+    {
+        //yield return new WaitForSeconds(2);
+        //cube.transform.eulerAngles += new Vector3(0, 0, 90);
+        yield return new WaitForSeconds(2);
+        EditorSceneManager.LoadScene("Edumall");
+        //cube.transform.eulerAngles += new Vector3(0, 0, 90);
+    }
 }
